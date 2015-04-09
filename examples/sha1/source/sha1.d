38a73337dd0323405fae63a91326ed6b0f23aded
Etienne Cimon <etcimon@gmail.com>
2015-04-09 09:13:52 -0400

    Update example and remove GNUMP

--- examples/sha1/source/sha1.d
+++ examples/sha1/source/sha1.d
@@ -1,15 +1,19 @@
 import botan.hash.sha2_32 : SHA256;
 import std.stdio;
 import std.digest.sha : sha256Of;
+import memutils.unique;
+import botan.libstate.lookup;
+
 void main() {
-	import botan.libstate.global_state;
-	globalState();
-	import botan.libstate.lookup;
-	import botan.hash.hash;
-	auto sha = retrieveHash("SHA-256").clone();
+	auto sha = retrieveHash("SHA-256").clone(); // on the GC
 	string test = "hello";
 	sha.update(test);
-	writeln(sha.finished()[]);
-
-	writeln(sha256Of(test));
+	writeln(sha.finished()[]); // from botan
+	writeln(sha256Of(test)); // from phobos
+	
+	auto skein = retrieveHash("Skein-512").clone().unique(); // RAII, ie. unique() encapsulates in Unique!HashFunction
+	skein.update(test);
+	writeln(skein.finished()[]);
+	skein.free();
+	assert(skein.isEmpty);
 }
